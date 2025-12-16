db37f06f31ba248966547f9a1f8e035071b0b914
Anila Nawaz <anilanawaz531@gmail.com>
2025-12-16 21:35:09 +0500

    trying to fix vercel build issues

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@': require('path').resolve(__dirname),
-    };
-    return config;
-  },
+  turbopack: {},
   images: {
     remotePatterns: [
       {
